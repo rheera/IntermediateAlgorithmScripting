989af520917dee7560d9141a79d55c20369311be
Raman Heera <rheera2@uwo.ca>
2020-06-26 23:14:32 -0400

    Create SeekAndDestroy.js
    - input: an array and n arguments
    - output: original array without the elements that are the same as any of the arguments

---
+++ SeekAndDestroy.js
@@ -0,0 +1,11 @@
+/*
+You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments.
+Remove all elements from the initial array that are of the same value as these arguments.
+ */
+
+function destroyer(arr, ...args) {
+    return arr.filter(x => args.includes(x) == false);
+}
+
+let ans = destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+console.log(ans);
